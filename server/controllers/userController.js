d2853eaca5031d96589a4b48066d4bf2d4e2073f
yuanyuan <silviaxieyy@gmail.com>
2024-06-19 15:19:50 +1000

    sign up method with bcrypt, backend.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,20 @@
 
-const user = require('../models/userModel')
+const User = require('../models/userModel')
 
 const loginUser = async (req, res) => {
   res.json({mssg: 'The user has logged in.'})
 }
 
 const signupUser = async (req, res) => {
-  res.json({mssg:'The user has signed up.'})
+  const { email, password } = req.body
+
+  try {
+    const user = await User.signup(email, password)
+    
+    res.status(200).json({email, user})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 }
 
 module.exports = {
