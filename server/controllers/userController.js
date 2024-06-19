00a290d9a32bdd52f489bb99c210c47a160ad4f6
yuanyuan <silviaxieyy@gmail.com>
2024-06-19 20:24:54 +1000

    add JWT in userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,9 @@
-
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '1d'})
+}
 
 const loginUser = async (req, res) => {
   res.json({mssg: 'The user has logged in.'})
@@ -11,7 +15,10 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, password)
     
-    res.status(200).json({email, user})
+    //create token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
   } catch (error) {
     res.status(400).json({error: error.message})
   }
