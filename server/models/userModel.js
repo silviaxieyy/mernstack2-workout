63f678cb8d50e75bd465b7570f70b747893bb17c
yuanyuan <silviaxieyy@gmail.com>
2024-06-19 15:44:44 +1000

    use validator to handle Email & Password Validation.

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -17,6 +18,25 @@ const userSchema = new Schema({
 
 //static signup method
 userSchema.statics.signup = async function (email, password) {
+  //Validation
+  if (!email || !password) {
+    throw Error('Both Email and Password fields are required.')
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error('Please use the right Email Address.')
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error(`The password is not strong enough.
+      A strong password typically includes:
+        --At least 8 characters.
+        --At least one lowercase letter.
+        --At least one uppercase letter.
+        --At least one number.
+        --At least one special character.`)
+  }
+
   const exists = await this.findOne({ email })
 
   if (exists) {
