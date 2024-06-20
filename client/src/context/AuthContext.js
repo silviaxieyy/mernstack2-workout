c1a38f2ec0032b9d876ff29d0c84169f377b7ed3
yuanyuan <silviaxieyy@gmail.com>
2024-06-20 10:36:55 +1000

    create AuthContext and useAuthContext hook.

---
+++ client/src/context/AuthContext.js
@@ -0,0 +1,15 @@
+import { Children, createContext, useReducer } from "react";
+
+export const AuthContext = createContext()
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null
+  })
+
+  return (
+    <AuthContext.Provider value={{...state, dispatch}}>
+      { children }
+    </AuthContext.Provider>
+  )
+}
