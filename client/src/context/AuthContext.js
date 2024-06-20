68b3c0b2790d925fba356df225b30c6fdcf8cf00
yuanyuan <silviaxieyy@gmail.com>
2024-06-20 10:45:08 +1000

    AuthContext with authReducer function.

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,6 +1,17 @@
-import { Children, createContext, useReducer } from "react";
+import { createContext, useReducer } from "react";
 
 export const AuthContext = createContext()
+
+export const authReducer = (state, action) => {
+  switch(action.type) {
+    case 'LOGIN':
+      return { user: action.payload }
+    case 'LOGOUT':
+      return { user: null }
+    default:
+      return state
+  }
+}
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
